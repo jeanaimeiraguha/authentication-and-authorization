b31f9236605e2a8bfbd6eac699019f41d92a6e68
IRAGUHA Jean  Aime <jeanaimeiraguha@gmail.com>
2025-08-06 09:59:52 +0200

    Backend

--- server.js
+++ server.js
@@ -1,57 +1,64 @@
-import mysql from "mysql"
-import express from "express"
-import cors from  "cors"
-// import express-session from "express"
-import session from "express-session"
-const app=express()
-app.use(cors())
-app.use(express.json())
-// Setup form data reading
-app.use(express.urlencoded({extended:true}))
+import mysql from "mysql";
+import express from "express";
+import cors from "cors";
+import session from "express-session";
 
-// Setup Session
+const app = express();
 
+// Enable CORS and JSON parsing
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// Setup session middleware
 app.use(session({
-    secret:"iraguha",
-    resave:false ,
-    saveUninitialized:false
+  secret: "iraguha",        // Used to sign the session ID cookie
+  resave: false,
+  saveUninitialized: false
+}));
 
-}))
+// Connect to MySQL database
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "backend"
+});
 
+db.connect((err) => {
+  if (err) {
+    console.log(" An error occurred while connecting to the DB");
+    console.log(err);
+  } else {
+    console.log(" DB connection successful");
+  }
+});
 
-const db =mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"backend"
-})
-db.connect((err)=>{
-    if(err){
-        console.log("Ann error occured while trying to connect to db");
+// Home route
+app.get("/", (req, res) => {
+  res.send("Hello dev");
+});
+
+// Login route
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+  db.query(sql, [username, password], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: "Login Failed", error: err });
     }
-    else{
-        console.log("Connection succeed");
+
+    if (results.length > 0) {
+      req.session.user = results[0];
+      return res.send(" You are now logged in");
+    } else {
+      return res.send(" Wrong credentials");
     }
-})
-app.get('/',(req,res)=>{
+  });
+});
 
-
-    res.send("Hello dev")
-})
-//route for login
-
-app.post('/login',(req,res)=>{
-    const {username,password}=req.body;
-    const sql ="SELECT * FROM users where username=? AND password=?";
-    db.query(sql,[username,password],(err,results)=>{
-
-        if(err) return res.status(404).json("LOgin Failed");
-
-    })
-})
-
-
-
-app.listen(3000,()=>{
-    console.log(`MY app is running on http://localhost:3000`)
-})
+// Start server
+app.listen(3000, () => {
+  console.log(" App is running at http://localhost:3000");
+});
